0b47a153e787bdf1024aa91e9a9e65132479df9f
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-24 20:43:07 +0300

    Заюзали цикл для переобра элементов массива

---
+++ lection_02/example_03/Program.cs
@@ -0,0 +1,18 @@
+﻿// 
+
+int [] array = {0, 24, -10, 131, 32, 95, -1, 0, 306};
+int n = array.Length;
+int find = 0;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прервывает цикл
+    }
+    index++;
+
+}
+
